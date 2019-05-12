e2ec3949f451959f31e1e16f30e3d393c75ac4e8
killMyTime <leiweijie1998@outlook.com>
2019-05-12 18:45:43 +0800

    将数据库放到一个辣鸡服务器的版本，跨域请求待完善（安全性），没有太多注释和单元测试的版本~~

--- src/main/java/com/adweb/adwebserver/domain/CourseRepository.java
+++ src/main/java/com/adweb/adwebserver/domain/CourseRepository.java
@@ -2,7 +2,7 @@ package com.adweb.adwebserver.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
+        import java.util.List;
 
 public interface CourseRepository extends CrudRepository<Course,Long> {
     public List<Course> getCoursesByTeacherId(int teacherId);
--- src/main/java/com/adweb/adwebserver/web/TeachersController.java
+++ src/main/java/com/adweb/adwebserver/web/TeachersController.java
@@ -16,38 +16,51 @@ public class TeachersController {
     TeacherRepository teacherRepository;
     @Autowired
     TeacherService teacherService;
+
     @GetMapping(path = "/all")
-    public @ResponseBody Iterable<Teacher> getTeachers(){
+    public @ResponseBody
+    Iterable<Teacher> getTeachers() {
         //This returns a JSON or XML with the teachers
         return teacherRepository.findAll();
     }
+
     /**
      * 这里稍作解释，只要前端传过来的数据和后端对象的数据名字一致就可以全部解析了
      * 用框架真的是懒，不过自动化解析也就很舒服
+     *
      * @param teacher 老师相关参数
      * @return teacher对象
      */
     @PostMapping(path = "/register")
-    public @ResponseBody Teacher register(@Valid Teacher teacher){
+    public @ResponseBody
+    Teacher register(@Valid Teacher teacher) {
         return teacherService.register(teacher);
     }
+
     @GetMapping(path = "/login")
-    public @ResponseBody Teacher login(@RequestParam int number,@RequestParam String password){
-        return teacherService.login(number,password);
+    public @ResponseBody
+    Teacher login(@RequestParam int number, @RequestParam String password) {
+        return teacherService.login(number, password);
     }
+
     @PostMapping(path = "/update")
-    public @ResponseBody Teacher update(@Valid Teacher teacher){
+    public @ResponseBody
+    Teacher update(@Valid Teacher teacher) {
         return teacherService.update(teacher);
     }
+
     @GetMapping(path = "/info")
-    public @ResponseBody Teacher getInfo(@RequestParam int teacherID){
-        Teacher teacher=new Teacher();
+    public @ResponseBody
+    Teacher getInfo(@RequestParam int teacherID) {
+        Teacher teacher = new Teacher();
         teacher.setTeacherId(teacherID);
         return teacherService.getTeacher(teacher);
     }
+
     @PostMapping(path = "/password")
-    public @ResponseBody boolean setPassword(@Valid Teacher teacher,@RequestParam String newPassword){
-        return teacherService.setPassword(teacher,newPassword);
+    public @ResponseBody
+    boolean setPassword(@Valid Teacher teacher, @RequestParam String newPassword) {
+        return teacherService.setPassword(teacher, newPassword);
     }
 
 }
