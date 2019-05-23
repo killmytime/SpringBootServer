eeca003457d8be0863a380441193ed7a36de4e65
lingfeng <16302010020@fudan.edu.cn>
2019-05-24 00:44:24 +0800

    随便写了点 学生端读取课程的部分

--- src/main/java/com/adweb/adwebserver/service/impl/StudentServiceImpl.java
+++ src/main/java/com/adweb/adwebserver/service/impl/StudentServiceImpl.java
@@ -1,10 +1,14 @@
 package com.adweb.adwebserver.service.impl;
 
 import com.adweb.adwebserver.domain.*;
+import com.adweb.adwebserver.domain.repository.CourseRepository;
 import com.adweb.adwebserver.domain.repository.StudentRepository;
+import com.adweb.adwebserver.service.CourseService;
 import com.adweb.adwebserver.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 关于身份校验的东西，之后再详细搞出一个解决方案
@@ -14,8 +18,8 @@ import org.springframework.stereotype.Service;
 public class StudentServiceImpl implements StudentService {
     @Autowired
     private StudentRepository studentRepository;
-    /*@Autowired
-    private CourseRepository courseRepository;*/
+    @Autowired
+    private CourseRepository courseRepository;
 
     @Override
     public Student login(String wechatId) {
@@ -41,6 +45,19 @@ public class StudentServiceImpl implements StudentService {
         student = studentRepository.getStudentByStudentId(studentID);
 
         return student;
+    }
+
+    //todo courseRepository里要实现下面两个方法
+    @Override
+    public List<Course> getAllCourse(int studentID) {
+        //return courseRepository.getCoursesByStudentId(studentID);
+        return null;
+    }
+
+    @Override
+    public Course getDetailCourse(int studentID, int courseID) {
+        //return courseRepository.getCoursesByStudentIDAndCourseID();
+        return null;
     }
 
 
--- src/main/java/com/adweb/adwebserver/web/StudentsController.java
+++ src/main/java/com/adweb/adwebserver/web/StudentsController.java
@@ -1,12 +1,15 @@
 package com.adweb.adwebserver.web;
 
+import com.adweb.adwebserver.domain.Course;
 import com.adweb.adwebserver.domain.Student;
 import com.adweb.adwebserver.domain.repository.StudentRepository;
+import com.adweb.adwebserver.service.CourseService;
 import com.adweb.adwebserver.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/students")
@@ -30,6 +33,26 @@ public class StudentsController {
     public @ResponseBody
     Student update(@Valid Student student){
         return studentService.update(student);
+    }
+
+    //todo 正在完善 根据学生的id获取该学生选的所有课
+    @GetMapping(path = "/allCourse")
+    public @ResponseBody
+    List<Course> allCourse(@RequestParam int studentID) {
+        return studentService.getAllCourse(studentID);
+    }
+
+    //todo 正在完善 根据具体课程的id获取具体的一门课 是否保留studentID以便获取用户学习情况等等？
+    @GetMapping(path = "/detailCourse")
+    public @ResponseBody Course detailCourse(@RequestParam int studentID, @RequestParam int courseID) {
+        return studentService.getDetailCourse(studentID, courseID);
+    }
+
+    //todo 正在完善 更新学生学习课程的信息 比如学习进度等
+    @PostMapping(path = "/updateCourse")
+    public @ResponseBody Course updateCourse(@RequestParam int studentID, @RequestParam int courseID) {
+        //return studentService.update(@Valid Course course)
+        return null;
     }
 
     @GetMapping(path = "/info")
